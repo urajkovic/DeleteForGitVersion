0312838cae38c83a61b0e98bf35673a189870d42
Uros Rajkovic <uros.rajkovic@edeja.com>
2018-10-24 10:10:11 +0200

    +semver: minor

--- ConsoleAppForGitVersion/ConsoleAppForGitVersion/Properties/AssemblyInfo.cs
+++ ConsoleAppForGitVersion/ConsoleAppForGitVersion/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyFileVersion("0.2.0.0")]
 
-[assembly: AssemblyInformationalVersion("0.2.0-alpha.14+Branch.dev.Sha.2a0f1e9331f271198fc51dbe64ce1851d14a9054")]
+[assembly: AssemblyInformationalVersion("0.2.0-alpha.15+Branch.dev.Sha.781c7c7f729ef6770c9b5313b6e2811fbb147a4f")]
