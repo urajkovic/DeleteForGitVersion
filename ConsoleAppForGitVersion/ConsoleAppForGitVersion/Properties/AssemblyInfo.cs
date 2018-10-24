376d6b867666eb370a089e8c383f549bb07e22ff
Uros Rajkovic <uros.rajkovic@edeja.com>
2018-10-24 11:27:25 +0200

    blah

--- ConsoleAppForGitVersion/ConsoleAppForGitVersion/Properties/AssemblyInfo.cs
+++ ConsoleAppForGitVersion/ConsoleAppForGitVersion/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+// [assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
 
-[assembly: AssemblyInformationalVersion("4.0.0+0.Branch.master.Sha.f3d741bc7543402567ec052a79bb66ae2e7d275a")]
+[assembly: AssemblyInformationalVersion("4.1.0-alpha.4+Branch.dev.Sha.55cf93c887b08765d2b05ed842eeaeb4021c313f")]
